1de45dcb727a535b3911c11fe4ef4fbd7640e805
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-09-08 13:36:16 +0000

    install Nextui and react-pdf ➕

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import {Providers} from "./providers";
+import Nav from './components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,10 +16,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className='light'>
       <body className={inter.className}>
-        {children}
+        <Providers>
+          <Nav />
+          {children}
+        </Providers>
       </body>
     </html>
+
   )
 }
