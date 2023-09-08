60437edbb2e44e119c03bd6174ec63b41f48a6a2
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-09-08 13:38:31 +0000

    add downloadeble resume ✅

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,46 @@
+"use client"
+
+import Forms from './components/Forms'
+import { useState } from 'react'
+import { Margin, usePDF } from "react-to-pdf";
+
+const initialData = {
+  fullName: '',
+  email: '',
+  phoneNumber: '',
+  address: ''
+}
+
 export default function Home() {
+  const [data, setData] = useState(initialData)
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value
+    })
+  }
+  const { toPDF, targetRef } = usePDF({
+    filename: "usepdf-example.pdf",
+    page: { margin: Margin.MEDIUM }
+  });
+  
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="min-h-screen">
+      <Forms
+        fullNameValue={data.fullName}
+        emailValue={data.email}
+        phoneNumberValue={data.phoneNumber}
+        addressValue={data.address}
+        handleChange={handleChange}
+        onPress={toPDF}
+      />
+      <div ref={targetRef} className="w-full p-6">
+        <h3 style={{ fontSize: '24px', fontWeight: 'bold' }}>{data.fullName}</h3>
+        <p>{data.address}</p>
+        <p>{data.email}</p>
+        <p>{data.phoneNumber}</p>
+      </div>
+    </main>
   )
 }
