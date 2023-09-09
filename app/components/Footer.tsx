1d34c0056745c4520fed4b22d9fcce1e9043dd95
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-09-09 16:12:59 +0000

    add Footer ✅

---
+++ app/components/Footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return(
+    <div className="w-full h-[80px] text-center flex items-center justify-center">
+      <p>Derajatul © 2023</p>
+    </div>
+  )
+}
+export default Footer
