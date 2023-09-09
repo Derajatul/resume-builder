779c7edeab17aea01e9b71ee35f17e9a21712361
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-09-09 12:31:46 +0000

    add download PDF button ✅

---
+++ app/components/DownloadPDF.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import {Button} from "@nextui-org/button";
+
+type DownloadPDFValues = {
+  onDownload: () => any;
+}
+
+const DownloadPDF = ({onDownload}: DownloadPDFValues) => {
+  return(
+    <div className="flex justify-between border max-w-[806px] bg-white rounded-lg p-6 mb-4 items-center">
+      <div className=" flex gap-5"> 
+      <Image src="/icons/megaphone.svg" width={30} height={30} style={{width:'auto',height:'auto'}} alt='' />
+      <div>
+        <h3>Apply to jobs with this resume</h3>
+        <p>Download this resume as a PDF and upload it when you apply to jobs.</p>
+      </div>  
+      </div>
+      <Button variant="ghost" radius="full" onPress={onDownload}>
+          Download as PDF
+      </Button>
+    </div>
+     
+  )
+}
+export default DownloadPDF
